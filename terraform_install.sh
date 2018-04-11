98e651251aa9b22f7210a6e77682feb4956382e5
manee2k6 <manee2k6@gmail.com>
2018-04-11 07:26:52 +0530

    Update terraform_install.sh

--- terraform_install.sh
+++ terraform_install.sh
@@ -1,6 +1,6 @@
 sudo apt-get install unzip
-wget  https://releases.hashicorp.com/terraform/0.11.5/terraform_0.11.5_linux_amd64.zip
-unzip terraform_0.11.5_linux_amd64.zip
+wget  https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
+unzip terraform_0.11.7_linux_amd64.zip
 mv terraform /usr/local/bin
-rm terraform_0.11.5_linux_amd64.zip
+rm terraform_0.11.7_linux_amd64.zip
 terraform --version
