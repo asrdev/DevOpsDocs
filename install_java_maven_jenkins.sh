3149ace864ce72f03fc7102590df256cf9b7ed7f
manee2k6 <manee2k6@gmail.com>
2018-05-24 09:15:56 +0530

    Update install_java_maven_jenkins.sh

--- install_java_maven_jenkins.sh
+++ install_java_maven_jenkins.sh
@@ -1,7 +1,7 @@
 #Install Java
 sudo apt-get update
 sudo apt-get install default-jdk
-echo JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64" > /etc/environment
+echo "JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"" >> /etc/environment
 source /etc/environment
 echo $JAVA_HOME
 
@@ -12,6 +12,7 @@ cd /opt/
 wget http://www-eu.apache.org/dist/maven/maven-3/3.5.3/binaries/apache-maven-3.5.3-bin.tar.gz
 sudo tar -xvzf apache-maven-3.5.3-bin.tar.gz
 sudo mv apache-maven-3.5.3 maven
+rm apache-maven-3.5.3-bin.tar.gz
 echo /etc/profile.d/mavenenv.sh > export M2_HOME=/opt/maven export PATH=${M2_HOME}/bin:${PATH}
 sudo chmod +x /etc/profile.d/mavenenv.sh
 sudo source /etc/profile.d/mavenenv.sh
